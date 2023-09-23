4c0a3ea9657f2de7fcfcf6473dd30ef3f2f87508
Jean-Christophe Antoine <jean-christophe.a@padoa-group.com>
2023-09-24 01:46:49 +0200

    feat(dev): preprocessing: noise reduction
    Signed-off-by: Jean-Christophe Antoine <jean-christophe.a@padoa-group.com>

--- src/audio/preprocessing/audio-enhance.py
+++ src/audio/preprocessing/audio-enhance.py
@@ -20,11 +20,23 @@ class ReduceNoise:
     def load(self) -> None:
         self.audio_data, self.sample_rate = librosa.load(self.input_audio_path, sr=None)
 
-    def reduce_noise(self) -> None:
+    def reduce_noise(self):
         self.reduced_audio_data = nr.reduce_noise(y=self.audio_data, sr=self.sample_rate)
+        return self
 
     def save_as_wav(self) -> None:
         sf.write(self.output_audio_path, self.reduced_audio_data, self.sample_rate)
 
     def save_as_torch_tensor(self) -> torch.Tensor:
         return torch.tensor(self.reduced_audio_data, dtype=torch.float32)
+
+
+def main():
+    for i in range(0, 105, 5):
+        rn = ReduceNoise(input_audio_path=f'./wavs/macron_split_{i}.wav', output_audio_path=f'./wavs/macron_split_{i}.wav')
+        rn.reduce_noise()
+        rn.save_as_wav()
+
+
+if __name__ == "__main__":
+    main()
